278c3b3a4c99247965d76582c1ee563eaf63858d
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-11-27 11:43:30 +0000

    modification: added some unit tests checking validity of nlogn values

--- unitTest.py
+++ unitTest.py
@@ -31,9 +31,29 @@ class my_unit_tests(unittest.TestCase):
             result = True
         self.assertEqual(result, True)
 
+    # ---------------------------------------------------------------------
+    # TESTING: plot_graph()
+    # ---------------------------------------------------------------------
+
+    # Test the sort has been triggered
+    def test_np_sort(self):
+        sorted_sample, nlogn = plot_graph(daily_change_array)
+        assert np.all(np.diff(sorted_sample) >= 0)
 
 
- 
+
+    # Test that nlogn has been calculated correctly
+    def test_nlogn_correct(self):
+        n_values = np.array([10, 20, 30]) # Checking with known vals
+        expected_result = np.array([
+        10 * np.log(10),
+        20 * np.log(20),
+        30 * np.log(30)
+    ])
+        actual_result = compute_nlogn(n_values)
+        assert np.allclose(expected_result, actual_result)
+
+
     # run the tests
 if __name__ == "__main__":
     unittest.main()
